30602d538dac9349458753d8c0d853f81d9defb4
mbarros46 <miguelbarrosramos46@gmail.com>
2025-09-22 11:50:09 -0300

    Remove arquivos obsoletos e substitui settings.gradle por settings.gradle.kts para melhor compatibilidade com Kotlin.

--- run-with-env.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-set GITHUB_ID=Ov23liLtdUza7rewjQ9r
-set GITHUB_SECRET=789d41d8d0d5dc7cdf74f1c44630160b4ba852e5
-set SPRING_DOCKER_COMPOSE_ENABLED=false
-echo Iniciando aplicacao com variaveis de ambiente configuradas...
-echo GITHUB_ID: %GITHUB_ID%
-echo Docker Compose desabilitado para evitar conflitos
-gradlew.bat bootRun
